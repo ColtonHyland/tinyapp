45c97b4ed98ffe3173e4e91ce057ee21de3eea64
Colton Hyland <colthyland@gmail.com>
2023-01-24 04:32:19 -0800

    id generating function added

--- express_server.js
+++ express_server.js
@@ -11,6 +11,15 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+  var chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  var charLength = chars.length;
+  var result = '';
+  for ( var i = 0; i < 6; i++ ) {
+     result += chars.charAt(Math.floor(Math.random() * charLength));
+  }
+  return result;
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
