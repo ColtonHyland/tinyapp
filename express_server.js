d8c2226c5c0140090d52366cc94fa938c5af1b87
Colton Hyland <colthyland@gmail.com>
2023-01-25 17:53:54 -0800

    Display login name

--- express_server.js
+++ express_server.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const cookieParser = require('cookie-parser')
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
-
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -44,16 +45,23 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  
+  const templateVars = { 
+    urls: urlDatabase, 
+    username: req.cookies["username"]
+  };
+  console.log(templateVars)
+  console.log(req.cookies)
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"]};
   res.render("urls_show", templateVars);
 });
 
@@ -65,6 +73,7 @@ app.get("/u/:id", (req, res) => {
 app.post("/login", (req, res) => {
   const username = res.cookie('username', req.body.username);
   //res.cookie('username', res.body);
+  
   res.redirect("/urls");
 });
 
